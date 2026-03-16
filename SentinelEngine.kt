2d5fbb395da0f0613a7ace33e8fee01af463d812
igorvolpe <142549377+igorvolpe@users.noreply.github.com>
2026-03-16 13:26:09 -0300

    Update SentinelEngine.kt

--- SentinelEngine.kt
+++ SentinelEngine.kt
@@ -1,7 +1,6 @@
 data class Telemetry(val deviceId: String, val temp: Double, val vibration: Double)
 
 class Analytics(private val data: List<Telemetry>) {
-    
     fun generateReport() {
         val avgTemp = data.map { it.temp }.average()
         val maxVib = data.maxOfOrNull { it.vibration } ?: 0.0
@@ -9,17 +8,16 @@ class Analytics(private val data: List<Telemetry>) {
         println("--- 📊 Relatório de Telemetria ---")
         println("Média de Temperatura: ${"%.2f".format(avgTemp)}ºC")
         println("Pico de Vibração: $maxVib")
-        println("Total de Dispositivos: ${data.size}")
         
-        if (avgTemp > 35.0) println("⚠️ Alerta: Tendência de aquecimento global na rede.")
+        if (avgTemp > 40.0) println("🚨 ALERTA: Sistema em sobreaquecimento!")
     }
 }
 
 fun main() {
     val logs = listOf(
-        Telemetry("SN-01", 32.5, 0.2),
-        Telemetry("SN-02", 45.0, 0.9),
-        Telemetry("SN-03", 38.2, 0.4)
+        Telemetry("SN-01", 35.5, 0.2),
+        Telemetry("SN-02", 48.0, 0.9), // Exemplo de alerta
+        Telemetry("SN-03", 32.1, 0.1)
     )
     Analytics(logs).generateReport()
 }
