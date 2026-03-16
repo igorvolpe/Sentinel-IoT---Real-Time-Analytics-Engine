4a28de4169e98e7be9ad77e973acd8b6d6e780ba
igorvolpe <142549377+igorvolpe@users.noreply.github.com>
2026-03-16 13:25:47 -0300

    Update 02_partitions.sql

--- 02_partitions.sql
+++ 02_partitions.sql
@@ -1,1 +1,5 @@
+CREATE TABLE telemetry_y2026m03 PARTITION OF device_telemetry
+    FOR VALUES FROM ('2026-03-01 00:00:00+00') TO ('2026-04-01 00:00:00+00');
 
+CREATE INDEX idx_telemetry_device_id_ts ON device_telemetry (device_id, ts DESC);
+CREATE INDEX idx_telemetry_payload_gin ON device_telemetry USING GIN (payload);
