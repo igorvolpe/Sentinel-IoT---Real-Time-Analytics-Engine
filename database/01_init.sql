9f12d886955ab55bf54cd658b4dbc4f1a97ba2d1
igorvolpe <142549377+igorvolpe@users.noreply.github.com>
2026-03-16 13:56:00 -0300

    Update 01_init.sql

--- database/01_init.sql
+++ database/01_init.sql
@@ -1,1 +1,18 @@
+CREATE TABLE customers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
+);
 
+CREATE TABLE devices (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    customer_id UUID REFERENCES customers(id),
+    serial_number TEXT UNIQUE NOT NULL,
+    device_type TEXT NOT NULL
+);
+
+CREATE TABLE device_telemetry (
+    device_id UUID NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
+    payload JSONB NOT NULL
+) PARTITION BY RANGE (ts);
